8208bfbe1a2823dfdbbc1d77f560d336a00d7413
geonetguy <41494452+geonetguy@users.noreply.github.com>
2026-04-19 17:18:01 -0700

    Remove build artifacts from repo

--- build/idcard_maker/windows/app/src/app/idcard_maker/__main__.py
+++
@@ -1,5 +0,0 @@
-# Entrypoint for `python -m idcard_maker` (used by `briefcase dev/run`)
-from .app import main
-
-if __name__ == "__main__":
-    main().main_loop()
--- build/idcard_maker/windows/app/src/app/idcard_maker/controllers/__init__.py
+++
@@ -1,2 +0,0 @@
-# idcard_maker/controllers/__init__.py
-from .actions import Actions
